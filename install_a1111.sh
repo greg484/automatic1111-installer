cc4a9d1902c7e7863346c9f86b79fb77bebeaac2
greg484 <gregory.7cats@gmail.com>
2025-06-12 08:05:14 +0900

    Create install_a1111.sh

---
+++ install_a1111.sh
@@ -0,0 +1,11 @@
+#AUTOMATIC1111
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /workspace/stable-diffusion-webui
+
+cd /workspace/stable-diffusion-webui
+
+# ControlNet 拡張をインストール
+git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
+
+# モデルの同期
+rclone sync gdrive:models_fb models/Stable-diffusion
+rclone sync gdrive:ControlNet extensions/sd-webui-controlnet/models
